e2808e9700edf96e6d06c42838df796b5a62a5e1
uncleoptimus <uncle.optimus@gmail.com>
2015-10-05 22:48:03 -0700

    added backbonefire after converting to npm module; implemented firebase Collections.

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,5 +1,12 @@
 
-import Backbone from 'backbone';
+/*
+project: hellows
+file: index.js
+author: Andrew Roy Chen
+last update: oct 3 2015
+*/
+import Firebase from 'firebase';
+var Backbone = require('backfire');
 import React from 'react.backbone'; // includes react
 
 // dummy data
@@ -13,15 +20,66 @@ var CARDS = [
 ];
 
 
+class Notepad extends React.Component {
+
+    get cardInfo() {
+      let phrase = document.querySelector('#phrase').value;
+      let origin = document.querySelector('#origin').value;
+      console.log('here is where a new card is made');
+      console.log('Greeting is %s and source is %s', phrase, origin);
+
+      return {
+        phrase: document.querySelector('#phrase').value,
+        origin: document.querySelector('#origin').value
+      };
+    }
+
+    makeNewCard() {
+
+      cardstack.add({
+        phrase: document.querySelector('#phrase').value,
+        origin: document.querySelector('#origin').value
+      });
+      document.querySelector('#phrase').value = '';
+      document.querySelector('#origin').value = '';
+    }
+
+    render() {
+      return (
+        <div className="notepad paper-bg paper">
+          <h2>rulez:</h2>
+          <ul className="notepad__rules">
+            <li>~40 letters above & below</li>
+            <li> <strong>be kind :)</strong></li>
+          </ul>
+          <p className="notepad__section">
+            <label>greetings:</label>
+            <input id="phrase" type="text" name="phrase"
+            placeholder="greetings!" required maxlength="40" />
+          </p>
+          <p className="notepad__section">
+            <label>from:</label>
+            <input id="origin" type="text" name="origin" placeholder="a friend"
+            required maxlength="30"/>
+          </p>
+          <button onClick={this.makeNewCard}>POST!</button>
+        </div>
+      );
+    }
+}
+
 class Card extends React.Component {
     render() {
         return (
-            <div className="welcome-card">
+          <div>
+            <a href="#" className="card-pin"></a>
+            <div className="welcome-card paper-bg paper">
                 <p>
                   <span className="welcome-phrase">{this.props.data.phrase}</span>
                 </p>
                 from <span className="welcome-origin">{this.props.data.origin}</span>
             </div>
+          </div>
         );
     }
 }
@@ -30,11 +88,14 @@ class Card extends React.Component {
 var Container = React.createBackboneClass ({
     render() {
         let all_cards = this.getCollection().map(card => {
-          console.dir(card);
+          // console.dir(card);
           return <Card data={card.attributes} />;
         });
         return (
+          <div>
+            <Notepad />
             <div className="card-board">{all_cards}</div>
+          </div>
         );
     }
 });
@@ -50,8 +111,12 @@ var CardData = Backbone.Model.extend({
 });
 
 // instantiate the collection and seed with data
-var cardstack = new Backbone.Collection({model:CardData});
-cardstack.set(CARDS);
+var CardStack = Backbone.Firebase.Collection.extend({
+  model: CardData,
+  url: 'https://hellows.firebaseio.com/',
+});
+var cardstack = new CardStack();
+// cardstack.add(CARDS);
 
 
 React.render(<Container collection={cardstack} />, document.querySelector('.container'));
